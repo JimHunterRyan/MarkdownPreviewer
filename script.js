eaeeb7762f5a04b180d9c4a1f1b18de50bafe05f
Jim Ryan <jimmih15@gmail.com>
2020-02-11 18:43:16 +0000

    added style

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+ 
+
+  /*
+        var input = this.document.getElementById("editor")
+        
+    var output = this.document.getElementById("preview")
+    
+    
+        
+    
+
+    input.addEventListener("onkeydown", func());
+        
+    function func(){
+        output.innerHTML=marked(input.innerHTML)
+    }*/
+};
