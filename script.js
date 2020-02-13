35cbc4148eccb6584ce8f859d8375bd3e2ceb5ad
Jim Ryan <jimmih15@gmail.com>
2020-02-13 16:08:16 +0000

    fixed update lag
    No longer lags 1 char behind

--- script.js
+++ script.js
@@ -1,64 +1,53 @@
-window.onload = function() {
-    
-    
 
+ 
+document.getElementById("editor").innerHTML =  `# Welcome to my Markdown Previewer!
+  
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+\`\`\`
+// this is multi-line code:
 
-  var input = this.document.getElementById("editor")
-  input.innerHTML= 
-  `# Welcome to my Markdown Previewer!
-  
-  ## This is a sub-heading...
-  ### And here's some other cool stuff:
-    
-  \`\`\`
-  // this is multi-line code:
-  
-  function anotherExample(firstLine, lastLine) {
-    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-      return multiLineCode;
-    }
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
   }
-  \`\`\`
-    
-  You can also make text **bold**... whoa!
-  Or _italic_.
-  Or... wait for it... **_both!_**
-  And feel free to go crazy ~~crossing stuff out~~.
+}
+\`\`\`
   
-  There's also [links](https://www.freecodecamp.com), and
-  > Block Quotes!
-  
-  And if you want to get really crazy, even tables:
-  
-  Wild Header | Crazy Header | Another Header?
-  ------------ | ------------- | ------------- 
-  Your content can | be here, and it | can be here....
-  And here. | Okay. | I think we get it.
-  
-  - And of course there are lists.
-    - Some are bulleted.
-       - With different indentation levels.
-          - That look like this.
-  
-  
-  1. And there are numbererd lists too.
-  1. Use just 1s if you want! 
-  1. But the list goes on...
-  - Even if you use dashes or asterisks.
-  * And last but not least, let's not forget embedded images:
-  
-  ![JavaScript Logo](images/js.png)`;
-        
-    var output = this.document.getElementById("preview")
-    
-    
-        
-    
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
 
-    input.addEventListener("onkeydown", func());
-        
-    function func(){
-        var newString = input.innerHTML
-        output.innerHTML = marked(newString);
-    }
-};
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![JavaScript Logo](images/js.png)`;
+
+
+document.getElementById("editor").addEventListener("onkeykeydown keypress keyuppress", update());
+
+function update() {
+  document.getElementById("preview").innerHTML = marked(document.getElementById("editor").value);
+    
+}
